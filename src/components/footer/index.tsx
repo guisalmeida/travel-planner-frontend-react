389811228272f7146792d62ee6ad15c2bd2710d4
guisalmeida <guisalmeida.dev@gmail.com>
2024-08-19 22:35:49 -0300

    chore: create footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,19 @@
+export function Footer() {
+  return (
+    <footer className="w-full max-w-6xl text-center space-y-10">
+      <small className="text-zinc-500 text-center w-full max-w-screen-md text-sm">
+        Ao planejar sua viagem pela plann.er você automaticamente concorda
+        <br />
+        com nossos{" "}
+        <a href="#" className="text-zinc-300 underline">
+          termos de uso
+        </a>{" "}
+        e{" "}
+        <a href="#" className="text-zinc-300 underline">
+          políticas de privacidade
+        </a>
+        .
+      </small>
+    </footer>
+  );
+}
