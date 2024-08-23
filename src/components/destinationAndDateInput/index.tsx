ca1758242206b349198d2c2ed8a499df978edf89
guisalmeida <guisalmeida.dev@gmail.com>
2024-08-23 00:47:38 -0300

    style: fix date picker

--- src/components/destinationAndDateInput/index.tsx
+++ src/components/destinationAndDateInput/index.tsx
@@ -4,6 +4,7 @@ import { Button } from "../button";
 import { Modal } from "../modal";
 import { DateRange, DayPicker, getDefaultClassNames } from "react-day-picker";
 import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import "react-day-picker/style.css";
 
 type DestinationAndDateInputProps = {
@@ -33,12 +34,9 @@ export function DestinationAndDateInput({
   let footer = `Please pick the first day.`;
   if (dateRange?.from) {
     if (!dateRange?.to) {
-      footer = format(dateRange.from, "d");
+      footer = format(dateRange.from, "d", { locale: ptBR });
     } else if (dateRange?.to) {
-      footer = `${format(dateRange?.from, "d ' de 'LLL")} até ${format(
-        dateRange?.to,
-        "d' de 'LLL"
-      )}`;
+      footer = `${format(dateRange?.from, "d ' de 'LLLL", { locale: ptBR })} até ${format(dateRange?.to,"d' de 'LLLL",{ locale: ptBR })}`;
     }
   }
 
@@ -65,7 +63,8 @@ export function DestinationAndDateInput({
         <Calendar className="size-5 text-zinc-400" />
         <span className="text-md text-zinc-400 w-56">
           {dateRange?.from && dateRange?.to
-            ? format(new Date(dateRange?.from as Date), "dd/MM/yyyy") + ' - ' +
+            ? format(new Date(dateRange?.from as Date), "dd/MM/yyyy") +
+              " - " +
               format(new Date(dateRange?.to as Date), "dd/MM/yyyy")
             : "Quando?"}
         </span>
@@ -80,14 +79,16 @@ export function DestinationAndDateInput({
               min={2}
               selected={dateRange}
               onSelect={setDateRange}
+              disabled={{ before: new Date() }}
+              excludeDisabled={true}
               footer={footer}
               classNames={{
                 today: `text-sky-300 font-semibold`,
                 chevron: `${defaultClassNames.chevron} fill-sky-300`,
-                selected: `${defaultClassNames.selected} bg-zinc-800 text-zinc-400`, // Highlight the selected day
-                range_start: `${defaultClassNames.range_start} bg-zinc-900 text-zinc-400`,
-                range_end: `${defaultClassNames.range_end} bg-zinc-900 text-zinc-400`,
-                day_button: `${defaultClassNames.day_button}`,
+                selected: `${defaultClassNames.selected} bg-zinc-800 text-zinc-400`,
+                range_start: `${defaultClassNames.range_start} bg-zinc-900`,
+                range_end: `${defaultClassNames.range_end} bg-zinc-900`,
+                day_button: `${defaultClassNames.day_button} hover:bg-sky-300 hover:text-zinc-800 border-sky-600 text-zinc-900`,
                 footer: `${defaultClassNames.footer} text-sky-300`,
               }}
               required
